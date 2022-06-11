2279476e41a634552b4d9e9850a38f7ca182bf40
FaustinoAndres <49380815+FaustinoAndres@users.noreply.github.com>
2022-06-11 18:28:32 -0400

    conver to csv to parquet

--- extract/extract.py
+++ extract/extract.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import subprocess
+import pandas as pd
 from subprocess import CompletedProcess
 from typing import List
 from zipfile36 import ZipFile
@@ -16,7 +17,9 @@ def extract(datasets: List[str]) -> None:
     #create_dir_to_save_dataset(datasets)
     #download_data_from_kaggle(datasets)
     #move_downloaded_zip_file(datasets)
-    extract_data_from_zip_file(datasets)
+    #extract_data_from_zip_file(datasets)
+    #remove_zip_files(datasets)
+    #convert_csv_to_parquet_format(datasets)
     logging.info('Finished extract process')
 
 def set_kaggle_config_dir_venv() -> None:
@@ -24,7 +27,7 @@ def set_kaggle_config_dir_venv() -> None:
     > Set the KAGGLE_CONFIG_DIR environment variable to .kaggle directory
     """
 
-    pwd = os.getcwd()
+    pwd: str = os.getcwd()
     logging.info('Setting KAGGLE_CONFIG_DIR')
     os.environ['KAGGLE_CONFIG_DIR'] = f'{pwd}/.kaggle'
 
@@ -105,3 +108,46 @@ def extract_zip_file(file_path: PosixPath) -> None:
             logging.info(f'Start to extract {member} file')
             zip.extract(member=member, path=path)
             logging.info(f'{member} file extracted successfully')
+
+def remove_zip_files(datasets: List[str]) -> None:
+    """
+    It takes a list of datasets, splits the dataset name, and then deletes the zip file
+
+    Args:
+        datasets (List[str]): List[str]
+    """
+
+    for dataset in datasets:
+        dataset = dataset.split('/')[1]
+        logging.info(f'{dataset}.zip will be deleted')
+        completed_process: CompletedProcess = subprocess.run(['find', '.', '-name', f'{dataset}.zip', '-delete'], capture_output=True)
+        capture_output(completed_process)
+        logging.info(f'{dataset} was deleted')
+
+def convert_csv_to_parquet_format(datasets: List[str]) -> None:
+    """
+    It takes a list of datasets, loops through each dataset, loops through each csv file in the dataset,
+    and then saves the csv file as a parquet file
+
+    Args:
+        datasets (List[str]): List[str]
+    """
+
+    for dataset in datasets:
+        dataset = dataset.split('/')[1]
+        dataset_path = DATA_RAW_DIR.resolve() / f'{dataset}'
+        csv_files: List[PosixPath] = list(dataset_path.glob('*.csv'))
+        for csv_file in csv_files:
+            save_parquet_file(csv_file)
+
+def save_parquet_file(csv_file: PosixPath) -> None:
+    """
+    > Read a CSV file, convert it to a Pandas DataFrame, and save it as a Parquet file.
+
+    Args:
+        csv_file (PosixPath): The path to the CSV file you want to convert to Parquet.
+    """
+
+    df = pd.read_csv(csv_file)
+    new_file: str = str(csv_file).replace('csv', 'parquet')
+    df.to_parquet(new_file, engine='fastparquet', index=False)
