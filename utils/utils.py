b15e155f492922bece73acc7c0b29d4414d862b2
FaustinoAndres <49380815+FaustinoAndres@users.noreply.github.com>
2022-06-11 01:52:24 -0400

    captures output

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,9 @@
-from utils.utils import *
+from subprocess import CompletedProcess
+import logging
+
+def capture_output(completed_process: CompletedProcess):
+
+    if len(completed_process.stdout) > 0:
+        logging.info(completed_process.stdout)
+    if len(completed_process.stderr) > 0:
+        logging.info(completed_process.stderr)
